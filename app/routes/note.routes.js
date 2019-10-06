476e7cdc1ac9da4952bdda12feed527db053a02f
Aakash kumar <aakash012@live.com>
2019-10-06 23:04:59 +0530

    Controller added

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -1,18 +1,18 @@
 module.exports = (app) => {
     const notes = require('../controllers/note.controller.js');
 
-    //create a new note
+    // Create a new Note
     app.post('/notes', notes.create);
 
-    //Retrieve all notes
+    // Retrieve all Notes
     app.get('/notes', notes.findAll);
 
-    //Retrieve a single note with noteId
+    // Retrieve a single Note with noteId
     app.get('/notes/:noteId', notes.findOne);
 
-    //Update a note with noteId
+    // Update a Note with noteId
     app.put('/notes/:noteId', notes.update);
 
-    //Delete a note
-    app.delete('notes/:noteId', notes.delete);
+    // Delete a Note with noteId
+    app.delete('/notes/:noteId', notes.delete);
 }
--- server.js
+++ server.js
@@ -1,34 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-//Configuting the database
-const dbConfig = require('./config/database.config');
+// create express app
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//Connecting to the database
+// Connecting to the database
 mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
+	useNewUrlParser: true
 }).then(() => {
-    console.log("Successfully connected to the database");
+    console.log("Successfully connected to the database");    
 }).catch(err => {
-    console.log('Could not connect to the database. Exiting now...',err);
+    console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
 });
-//Create Express app
-const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
-
-app.use(bodyParser.json())
-
-//define a simple route
+// define a simple route
 app.get('/', (req, res) => {
-    res.json({"Message": "Welcome to EasyNotes Application. Take notes quickly. Organize and keep track of all your notes."});
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
-//listen for requests
+require('./app/routes/note.routes.js')(app);
+
+// listen for requests
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
